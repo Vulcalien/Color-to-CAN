02619f7f80c588e1c5481364b26f0c04ab3c9e0a
Vulcalien <vulcalien@vulcalien.net>
2025-12-13 12:41:10 +0100

    Interface: add a draft of the 'color2can' interface

---
+++ firmware/apps/color/src/checks.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include "color2can.h"
+
+// Compile-time checks
+
+_Static_assert(
+    sizeof(struct color2can_config) == COLOR2CAN_CONFIG_SIZE,
+    "size of struct color2can_config is incorrect"
+);
+
+_Static_assert(
+    sizeof(struct color2can_sample) == COLOR2CAN_SAMPLE_SIZE,
+    "size of struct color2can_sample is incorrect"
+);
---
+++ include/color2can.h
@@ -0,0 +1,50 @@
+/* Copyright 2025 Vulcalien
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define COLOR2CAN_CONFIG_SIZE 2
+struct color2can_config {
+    uint8_t frequency;           // 0=only on-demand, 1...60 Hz
+    uint8_t use_led         : 2; // 0=never, 1=when sampling, 2=always on
+    uint8_t transmit_timing : 1; // 0=on-demand, 1=continuous
+};
+
+#define COLOR2CAN_SAMPLE_SIZE 4
+struct color2can_sample {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t clear;
+};
+
+// TODO are the numbers and IDs below good?
+
+// number of distinct sensor IDs (ID=0 is broadcast)
+#define COLOR2CAN_MAX_SENSOR_COUNT 32
+
+#define COLOR2CAN_CONFIG_MASK_ID 0x720 // 0x720...0x73f
+#define COLOR2CAN_SAMPLE_MASK_ID 0x740 // 0x740...0x75f
+
+#ifdef __cplusplus
+}
+#endif
